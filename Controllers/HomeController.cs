85cd9dd1cb866267a97b4ba30130916abb717db2
Sahaj Jain <sjai0024@student.monash.edu>
2023-04-28 12:35:46 +1000

    Link to quiz feedback API

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Diagnostics;
+using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using NewsWise.Models;
+using Newtonsoft.Json;
 
 namespace NewsWise.Controllers
 {
@@ -89,31 +91,31 @@ namespace NewsWise.Controllers
             return View(claimReview);
         }
 
-        //public IActionResult Search(string searchString)
-        //{
-        //    ClaimReview match = null;
-        //    if (!searchString.IsNullOrEmpty())
-        //    {
-        //        List<ClaimReview> articles = _context.Review.ToList();
-        //        foreach (var article in articles)
-        //        {
-        //            if (article.Title != null)
-        //                if (article.Title.ToUpper().Contains(searchString.ToUpper()))
-        //                {
-        //                    match = article;
-        //                    break;
-        //                }
-        //        }
-        //    }
-        //    if (match != null)
-        //    {
-        //        return RedirectToAction("Details", "ClaimReviews", new { id = match.ClaimReviewId });
-        //    }
-        //    else
-        //    {
-        //        return RedirectToAction("Index", "Home");
-        //    }
-        //}
+        public IActionResult Search(string searchString)
+        {
+            ClaimReview match = null;
+            if (!searchString.IsNullOrEmpty())
+            {
+                List<ClaimReview> articles = _context.Review.ToList();
+                foreach (var article in articles)
+                {
+                    if (article.Title != null)
+                        if (article.Title.ToUpper().Contains(searchString.ToUpper()))
+                        {
+                            match = article;
+                            break;
+                        }
+                }
+            }
+            if (match != null)
+            {
+                return RedirectToAction("Details", "ClaimReviews", new { id = match.ClaimReviewId });
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
 
         [HttpGet]
         public IActionResult Autofill()
@@ -135,13 +137,15 @@ namespace NewsWise.Controllers
         {
             return View();
         }
+
         [HttpPost]
-        public IActionResult SubmitQuiz()
+        public async Task<IActionResult> SubmitQuizAsync()
         {
             var score = 0;
-
+            var chosenOption = new List<string>();
             // Check answer for Question 1
             var q1Answer = Request.Form["One"];
+            chosenOption.Add(q1Answer);
             if (q1Answer == "B")
             {
                 score += 1;
@@ -149,42 +153,115 @@ namespace NewsWise.Controllers
 
             // Check answer for Question 2
             var q2Answer = Request.Form["Two"];
+            chosenOption.Add(q2Answer);
             if (q2Answer == "B")
             {
                 score += 1;
             }
             var q3Answer = Request.Form["Three"];
+            chosenOption.Add(q3Answer);
             if (q3Answer == "C")
             {
                 score += 1;
             }
             // Display result
             var q4Answer = Request.Form["Four"];
+            chosenOption.Add(q4Answer);
             if (q4Answer == "B")
             {
                 score += 1;
             }
             var q5Answer = Request.Form["Five"];
+            chosenOption.Add(q5Answer);
             if (q5Answer == "B")
             {
                 score += 1;
             }
             var q6Answer = Request.Form["Six"];
+            chosenOption.Add(q6Answer);
             if (q6Answer == "B")
             {
                 score += 1;
             }
             var q7Answer = Request.Form["Seven"];
+            chosenOption.Add(q7Answer);
             if (q7Answer == "A")
             {
                 score += 1;
             }
             var q8Answer = Request.Form["Eight"];
+            chosenOption.Add(q8Answer);
             if (q8Answer == "A")
             {
                 score += 1;
             }
-            return View("QuizResults",score);
+            var answerList = new List<int>();
+            foreach (string answer in chosenOption)
+            {
+                switch (answer)
+                {
+                    case "A":
+                        answerList.Add(0);
+                        break;
+                    case "B":
+                        answerList.Add(1);
+                        break;
+                    case "C":
+                        answerList.Add(2);
+                        break;
+                }
+            }
+            // Prepare quiz data to send to Flask API
+            var quizData = new
+            {
+                q1 = "MC1",
+                q2 = "MC3",
+                q3 = "MC6",
+                q4 = "MC8",
+                q5 = "MC5",
+                q6 = "A1",
+                q7 = "A2",
+                q8 = "A3",
+                a1 = answerList[0],
+                a2 = answerList[1],
+                a3 = answerList[2],
+                a4 = answerList[3],
+                a5 = answerList[4],
+                a6 = answerList[5],
+                a7 = answerList[6],
+                a8 = answerList[7]
+            };
+
+            // Send quiz data to Flask API and receive feedback
+            var client = new HttpClient();
+            var response = await client.PostAsJsonAsync("https://newswiseapi.azurewebsites.net/quiz_feedback", quizData);
+            var feedbackJson = await response.Content.ReadAsStringAsync();
+            var feedback = JsonConvert.DeserializeObject<dynamic>(feedbackJson);
+            string SuggestedPage = feedback["Suggested Page"];
+            switch (SuggestedPage)
+            {
+                case "The definition of fake news":
+                    SuggestedPage = "Definition";
+                    break;
+                case "The existence of fake news":
+                    SuggestedPage = "Existence";
+                    break;
+                case "The importance of identifying fake news":
+                    SuggestedPage = "Importance";
+                    break;
+                case "Tips to spot fake news":
+                    SuggestedPage = "SpotTip";
+                    break;
+                case "Real life examples":
+                    SuggestedPage = "RealLifeExamples";
+                    break;
+            }
+
+            // Display result and feedback
+            return View("QuizResults", new { Score = score, Feedback = feedback.Feedback, SuggestedPage });
+
+
+            //return View("QuizResults",score);
         }
         public IActionResult QuizContent()
         {
